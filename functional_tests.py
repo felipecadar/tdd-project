df01ff2c19ae8a5e98ae34136f36b969f6fd5fe2
Felipe Cadar Chamone <cadar@dcc.ufmg.br>
2021-02-25 18:27:13 -0300

    Refatorando o Teste Funcional

--- functional_tests.py
+++ functional_tests.py
@@ -25,17 +25,22 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')  
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
-        # Ela digita "Estudar testes funcionais" em uma caixa de texto
-        inputbox.send_keys('Estudar testes funcionais')
-
         # Quando ela aperta enter, a página atualiza, e mostra a lista
         # "1: Estudar testes funcionais" como um item da lista TODO
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1: Estudar testes funcionais')
         
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')  
-        self.assertIn('1: Estudar testes funcionais', [row.text for row in rows])
+        # Ainda existe uma caixa de texto convidando para adicionar outro item
+        # Ela digita: "Estudar testes de unidade"
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        inputbox.send_keys('Estudar testes de unidade')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # A página atualiza novamente, e agora mostra ambos os itens na sua lista
+        self.check_for_row_in_list_table('1: Estudar testes funcionais')
+        self.check_for_row_in_list_table('2: Estudar testes de unidade')
         
         # Ainda existe uma caixa de texto convidando para adicionar outro item
         # Ela digita: "Estudar testes de unidade"
@@ -56,6 +61,11 @@ class NewVisitorTest(unittest.TestCase):
         # Ela visita a URL: a sua lista TODO ainda está armazenada
 
         # Satisfeita, ela vai dormir
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
 if __name__ == '__main__':
     unittest.main()
